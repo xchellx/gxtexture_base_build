dfcb1f4c2dced456e4e1d52a0756cb1e5f0a9035
xchellx <jon.xjonx.nat@gmail.com>
2024-12-21 07:42:06 -0500

    Fix copylibs.sh

--- copylibs.sh
+++ copylibs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 dp0="$(dirname $(readlink -m $BASH_SOURCE))"
-[ -n "$1" ] && [ -d "$1" ] && outdir="$1" || outdir="$dp0/build"
-[ -n "$2" ] && [ -d "$2" ] && _MSYS_PATH="$2" || _MSYS_PATH="/c/msys64/clang64/bin"
+if [ -n "$1" ] && [ -d "$1" ] ; then outdir="$1" ; else outdir="$dp0/build" ; fi
+if [ -n "$2" ] && [ -d "$2" ] ; then _MSYS_PATH="$2" ; else _MSYS_PATH="/c/msys64/clang64/bin" ; fi
 echo "Copying $_MSYS_PATH/libc++.dll to $outdir/libc++.dll..."
 cp "$_MSYS_PATH/libc++.dll" "$outdir/libc++.dll"
